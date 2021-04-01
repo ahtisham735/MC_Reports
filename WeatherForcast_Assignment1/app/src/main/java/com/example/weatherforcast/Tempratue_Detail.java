b83873adc4001dc84a7894a36b3ac6f5983323f7
Ahtisham <sahtisham735@gmail.com>
2021-04-01 09:12:34 +0500

    screens

---
+++ WeatherForcast_Assignment1/app/src/main/java/com/example/weatherforcast/Tempratue_Detail.java
@@ -0,0 +1,14 @@
+package com.example.weatherforcast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tempratue_Detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tempratue__detail);
+    }
+}
